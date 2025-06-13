31d40f2fc6348f2b6c7b41891441d615c6d1ae6c
stephanysouzasantana <reissantanasouza07@gmail.com>
2025-06-13 14:45:42 -0300

    teste

---
+++ Exercicios/src/PraticaGuiada1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PraticaGuiada1 {
+    public static void main(String[] args){
+        int soma =0;
+        for (int i=1; i<=5; i++){
+            soma +=i;
+        }
+         System.out.println("A soma dos cinco primeiros números inteiros positivos é:" +soma);
+
+    }
+}
